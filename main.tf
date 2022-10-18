e1a9819220ade791b83be8ca6a9a6af37ee0d7a6
pkzadeh <pkzadeh@gmail.com>
2022-10-18 07:11:53 +0100

    commited code

--- main.tf
+++ main.tf
@@ -1,12 +1,65 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
-resource "aws_instance" "example_server" {
-    ami           = "ami-0fb653ca2d3203ac1"
-    instance_type = "t2.micro"
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
-    tags = {
-        Name = "Terraform Example"
-    }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.port_number} &
+              EOF
+
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.port_number
+    to_port     = var.port_number
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+variable "port_number" {
+  type        = number
+  default     = 8080
+  description = "The port number"
+}
+
+
+
